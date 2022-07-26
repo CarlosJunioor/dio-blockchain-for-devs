2bac41e78e05406d512011fa5d8beaa35c2d93e5
CarlosJunioor <carlosjunior866@gmail.com>
2022-07-26 05:54:48 +0100

    update

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.5.0;
 contract Adoption{
     address[16]  public adopters;
 
-    function adopt(uint carsId) public returns (uint){
-        adopters[carsId] = msg.sender;
+    function adopt(uint petId) public returns (uint){
+        adopters[petId] = msg.sender;
 
-        return carsId;
+        return petId;
     }
 
     function getAdopters() public returns (address[16] memory){
